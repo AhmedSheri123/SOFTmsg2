b3317ca0ae61de4fce3116a94d07c080ec43aab9
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2024-11-23 13:58:29 +0300

    add robots.txt

--- softmsg/urls.py
+++ softmsg/urls.py
@@ -19,8 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.base import TemplateView
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+        path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+] + i18n_patterns(
     path('', include('pages.urls')),
     path('resources/', include('resources.urls')),
     path('accounts/', include('accounts.urls')),
